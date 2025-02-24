2c824c9570cd4ac6bf4623658d4132a7dd71b6b8
Ahmed Bahaaeddin Moharram <abmoharram@gmail.com>
2025-02-24 10:46:28 +0000

    To library

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import find_packages, setup
 setup(
     name='abm_dommon_functions',
     packages=find_packages(include=['src']),
-    version='0.1.0',
+    version='0.1.1',
     description='ABM common_functions',
     author='Me',
     install_requires=[],
     setup_requires=['pytest-runner'],
-    tests_require=['pytest==4.4.1'],
+    tests_require=['pytest'],
     test_suite='tests',
 )
