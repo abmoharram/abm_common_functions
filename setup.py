b74209b4edd12f4f755c73d08423a0816e3eed87
Ahmed Bahaaeddin Moharram <abmoharram@gmail.com>
2025-02-24 22:41:25 +0000

    To library

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="abm_common_functions",
-    packages=find_packages(include=[]),
+    packages=find_packages(include=["abm_common_functions"]),
     version="0.1.1",
     description="ABM common functions",
     authors=["Ahmed Moharram"],
